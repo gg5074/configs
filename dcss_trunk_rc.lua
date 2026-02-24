1a3df926ed3629e94fbd510bd4b1567f5173d043
gg5074 <30966025+gg5074@users.noreply.github.com>
2026-02-24 16:03:23 +0900

    Update dcss_trunk_rc.lua

--- dcss_trunk_rc.lua
+++ dcss_trunk_rc.lua
@@ -245,7 +245,7 @@ brc_config_explicit = {
         "ring of", "amulet of", "6 ring of strength", "6 ring of dexterity", "dragonskin cloak", "moon troll leather armour", "Cigotuvi's embrace",
         "spear of", "trident of", "partisan", "partisan of", "demon trident", "demon trident of", "trishula", "glaive", "bardiche",
         "broad axe", "morningstar", "eveningstar", "demon whip", "sacred scourge", "demon blade",
-        --"hand cannon", "orcbow",  "arbalest", "longbow", "triple crossbow",
+        --"hand cannon", "orcbow", "arbalest", "longbow", "triple crossbow",
         "buckler", "buckler of", "kite shield", "kite shield of", "tower shield", "tower shield of", "wand of digging", "quill talisman", "medusa talisman",
         "ring mail of", "scale mail of", "chain mail", "chain mail of", "plate armour", "plate armour of",
         "crystal plate armour", "golden dragon scales", "storm dragon scales", "swamp dragon scales",
