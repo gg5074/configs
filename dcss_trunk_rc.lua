96a1c69e19074efcc3089e320088328a047b2b38
gg5074 <30966025+gg5074@users.noreply.github.com>
2026-02-28 10:26:58 +0900

    Update dcss_trunk_rc.lua

--- dcss_trunk_rc.lua
+++ dcss_trunk_rc.lua
@@ -137,17 +137,8 @@ brc_config_explicit = {
     -- No config; See answer-prompts.lua for Questions/Answers
   },
 
-  ["announce-items"] = {
-    disabled = true, -- Disabled by default. Intended only for turncount runs.
-    announce_class = { "book", "gold", "jewellery", "misc", "missile", "potion", "scroll", "wand" },
-    announce_glowing = true,
-    announce_artefacts = true,
-    max_gold_announcements = 3, -- Stop announcing gold after 3rd pile on screen
-    announce_extra_consumables_wo_id = true, -- Announce when standing on not-id'd duplicates
-  },
-
   ["display-realtime"] = {
-    disabled = false, -- Disabled by default
+    disabled = true, -- Disabled by default
     interval_s = 300, -- seconds between updates
     emoji = "🕒",
     init = function()
@@ -426,7 +417,7 @@ sound_pack += https://osp.nemelex.cards/build/latest.zip:["init.txt"]
 
 sound_pack += https://sound-packs.nemelex.cards/DCSS-UST/v1.0.1.zip
 
-record_wtrec = true
+# record_wtrec = true
 
 #######################
 ### Mini Map Colors ###
@@ -4945,130 +4936,6 @@ end
 
 }
 ############################### End lua/features/announce-hp-mp.lua ###############################
-###################################################################################################
-
-############################## Begin lua/features/announce-items.lua ##############################
-######## https://github.com/brianfaires/crawl-rc/blob/main/lua/features/announce-items.lua ########
-{
----------------------------------------------------------------------------------------------------
--- BRC feature module: announce-items
--- @module f_announce_items
--- Announce when items of certain classes come into view. Off by default.
--- Intended and configured for turncount runs, to avoid having to manually check floor items.
----------------------------------------------------------------------------------------------------
-f_announce_items = {}
-f_announce_items.BRC_FEATURE_NAME = "announce-items"
-f_announce_items.Config = {
-  disabled = true, -- Disabled by default. Intended only for turncount runs.
-  announce_class = { "book", "gold", "jewellery", "misc", "missile", "potion", "scroll", "wand" },
-  announce_glowing = true,
-  announce_artefacts = true,
-  max_gold_announcements = 3, -- Stop announcing gold after 3rd pile on screen
-  announce_duplicate_consumables = true, -- Announce when standing on not-id'd duplicates
-} -- f_announce_items.Config (do not remove this comment)
-
----- Local constants ----
-local ALERT_COLOR = {
-  gold = BRC.COL.yellow,
-  book = BRC.COL.lightcyan,
-  jewellery = BRC.COL.magenta,
-  misc = BRC.COL.lightcyan,
-  missile = BRC.COL.white,
-  potion = BRC.COL.lightgreen,
-  scroll = BRC.COL.lightgreen,
-  wand = BRC.COL.magenta,
-  default = BRC.COL.lightblue,
-} -- ALERT_COLOR (do not remove this comment)
-
----- Local variables ----
-local C -- config alias
-local los_items
-local prev_item_names
-local prev_gold_count
-
----- Initialization ----
-function f_announce_items.init()
-  C = f_announce_items.Config
-  los_items = {}
-  prev_item_names = {}
-  prev_gold_count = 0
-end
-
----- Local functions ----
-local function should_announce_item(it)
-  if it.is_useless then return false end
-  if not it.is_identified then
-    if it.artefact then return C.announce_artefacts end
-    if it.branded then return C.announce_glowing end
-  elseif crawl.messages(2):contains(it.name()) then
-    -- Avoid duplicating "You see here ..." after floor-id
-    return false
-  end
-
-  return util.contains(C.announce_class, it.class(true):lower())
-end
-
-local function announce_item(it)
-  if not should_announce_item(it) then return end
-  local class = it.class(true):lower()
-  if class == "gold" then
-    prev_gold_count = prev_gold_count + 1
-    if prev_gold_count > C.max_gold_announcements then return end
-  end
-
-  local item_col = ALERT_COLOR[class] or ALERT_COLOR.default
-  crawl.mpr(BRC.txt.white("Found: ") .. BRC.txt[item_col](it.name()))
-  you.stop_activity()
-end
-
----- Crawl hook functions ----
-function f_announce_items.ready()
-  los_items = {}
-  local r = you.los()
-  for x = -r, r do
-    for y = -r, r do
-      if you.see_cell(x, y) then
-        local items_xy = items.get_items_at(x, y)
-        if items_xy and #items_xy > 0 then
-          for _, it in ipairs(items_xy) do
-            los_items[#los_items+1] = it
-
-            if C.announce_duplicate_consumables then
-              if x == 0 and y == 0 and not it.is_identified
-                and (it.class(true) == "scroll" or it.class(true) == "potion")
-               then
-                if util.exists(items.inventory(), function(i)
-                  return i.name("qual", false) == it.name("qual", false)
-                end) then
-                  crawl.mpr(BRC.txt.magenta("Duplicate: ") .. it.name())
-                end
-              end
-            end
-          end
-        end
-      end
-    end
-  end
-
-  for _, it in ipairs(los_items) do
-    if not util.contains(prev_item_names, it.name()) then
-      announce_item(it)
-    end
-  end
-
-  -- Save history for comparison
-  prev_item_names = {}
-  prev_gold_count = 0
-  for _, it in ipairs(los_items) do
-    prev_item_names[#prev_item_names+1] = it.name()
-    if it.class(true):lower() == "gold" then
-      prev_gold_count = prev_gold_count + 1
-    end
-  end
-end
-
-}
-############################### End lua/features/announce-items.lua ###############################
 ###################################################################################################
 
 ############################## Begin lua/features/answer-prompts.lua ##############################
